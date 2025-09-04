a946ce00187c1d468e47f9846ae3352afe3d58b5
caionicolini <152920265+hznqs@users.noreply.github.com>
2025-09-04 18:51:11 -0300

    Add files via upload

---
+++ ExercicioMedia/src/Media.java
@@ -0,0 +1,15 @@
+
+public class Media {
+    public static void main(String[] args) {
+
+        double num1 = 10;
+        double num2 = 8;
+        double num3 = 4.9;
+        double num4 = 4.8;
+        double media = (num1 + num2 + num3 + num4) / 4;
+
+        System.out.println("A média é: " + media);
+
+
+        }
+    }
