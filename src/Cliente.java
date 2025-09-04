3ff0485a769d80faef6cafdfe8001019665a1235
hznqs <caio.nicolini09@gmail.com>
2025-09-04 20:21:13 -0300

    tarefa wrapper

---
+++ src/Cliente.java
@@ -0,0 +1,27 @@
+import java.nio.charset.CharsetEncoder;
+
+public class Cliente {
+
+    private Integer codigo;
+
+    private Long codigoMaior = 1204050l;
+
+    private Double valorDecimal = 10.1;
+
+    private Boolean status = false;
+
+    private Float valoDecimal = 10.2f;
+
+    private Short shor;
+
+    private Byte bi;
+
+    private Character letra = 'A';
+
+
+    public static void main(String[] args){
+
+    }
+
+
+}
