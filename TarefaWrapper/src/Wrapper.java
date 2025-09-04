75ff70bd52137f2995ad2d02e35fb5e6a5214f97
hznqs <caio.nicolini09@gmail.com>
2025-09-04 20:08:28 -0300

    Tarefa Wrapper

---
+++ TarefaWrapper/src/Wrapper.java
@@ -0,0 +1,11 @@
+
+public class Wrapper {
+    public static void main(String[] args) {
+
+        int num1 = 1;
+        System.out.println(num1);
+        int numI = Integer.valueOf(num1);
+        System.out.println(numI);
+
+        }
+    }
